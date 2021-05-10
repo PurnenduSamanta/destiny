46ff1b6ae0fc9af0e1ee9c91364f607d72830afc
purnendu samanta <joysamanta123@gmail.com>
2021-05-10 20:08:22 +0530

    Initial commit

---
+++ lib/story.dart
@@ -0,0 +1,12 @@
+class Story
+{
+  String storyTitle;
+  String choice1,choice2;
+
+  Story({String storyTitle,String choice1,String choice2})
+  {
+    this.storyTitle=storyTitle;
+    this.choice1=choice1;
+    this.choice2=choice2;
+  }
+}
